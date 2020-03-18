8e6a802788cab03751e1b01e347c1c569833a9f2
thisrabbit <1195398892@qq.com>
2020-03-18 23:57:14 +0800

    the UI of popup is basically done.

--- src/pages/popup/index.tsx
+++ src/pages/popup/index.tsx
@@ -1,9 +1,53 @@
 import React, { useEffect, useState } from 'react';
-import { Typography, Progress, Tooltip, Button } from 'antd';
+import {
+  Typography,
+  Progress,
+  Tooltip,
+  Button,
+  Card,
+  Statistic,
+  Dropdown,
+  Menu,
+} from 'antd';
+import { SettingOutlined, PieChartOutlined } from '@ant-design/icons';
 import styles from './index.less';
 import { browser } from 'webextension-polyfill-ts';
-import { getDomainState, getUrlInfoInActiveTab } from '@/pages/background';
+import {
+  getDomainState,
+  getUrlInfoInActiveTab,
+  urlInfo,
+  domainState,
+} from '@/utils/common';
 import { tooltipWidth } from '@/utils/vars';
+
+const minutes2display: (minutes: number) => JSX.Element = minutes => {
+  return (
+    <span>
+      {minutes > 59 ? (
+        <>
+          <span>{Math.floor(minutes / 60)}</span>
+          <span className={styles['time-unit']}>
+            {' '}
+            {browser.i18n.getMessage('hour')}{' '}
+          </span>
+          <span>{minutes % 60}</span>
+          <span className={styles['time-unit']}>
+            {' '}
+            {browser.i18n.getMessage('minute')}{' '}
+          </span>
+        </>
+      ) : (
+        <>
+          <span>{minutes > 0 ? minutes : 0}</span>
+          <span className={styles['time-unit']}>
+            {' '}
+            {browser.i18n.getMessage('minute')}{' '}
+          </span>
+        </>
+      )}
+    </span>
+  );
+};
 
 export default () => {
   const [urlInfo, setUrlInfo] = useState();
@@ -29,10 +73,32 @@ export default () => {
             <Progress
               type="dashboard"
               width={tooltipWidth - 32 * 2}
-              percent={0}
+              status="active"
+              percent={
+                domainState.tracked
+                  ? domainState.limited
+                    ? ((domainState.maxLimitTime -
+                        domainState.currentlyUsedTime) /
+                        domainState.maxLimitTime) *
+                      100
+                    : (domainState.currentlyUsedTime / 60) * 100
+                  : 0
+              }
               format={() =>
                 domainState.tracked ? (
-                  'j'
+                  domainState.limited ? (
+                    <>
+                      <span className={styles['time-category']}>
+                        {browser.i18n.getMessage('timeLeftShort')}
+                      </span>
+                      {minutes2display(
+                        domainState.maxLimitTime -
+                          domainState.currentlyUsedTime,
+                      )}
+                    </>
+                  ) : (
+                    <span>{browser.i18n.getMessage('domainNotLimited')}</span>
+                  )
                 ) : (
                   <Tooltip
                     title={browser.i18n.getMessage('domainNotTrackedTooltip')}
@@ -43,9 +109,79 @@ export default () => {
               }
             />
           </div>
-          <div>
-            <Button>加入统计</Button>
+          <div className={styles['options']}>
+            {domainState.tracked ? (
+              domainState.limited ? (
+                <Dropdown
+                  placement="bottomCenter"
+                  overlay={
+                    <Menu style={{ textAlign: 'center' }}>
+                      <Menu.Item key="1">
+                        <Button danger>
+                          {browser.i18n.getMessage('removeDomainFromLimitList')}
+                        </Button>
+                      </Menu.Item>
+                      <Menu.Item key="2">
+                        {browser.i18n.getMessage('removeDomainFromTrackList')}
+                      </Menu.Item>
+                    </Menu>
+                  }
+                >
+                  <Button>{browser.i18n.getMessage('addMoreTime')}</Button>
+                </Dropdown>
+              ) : (
+                <Button>
+                  {browser.i18n.getMessage('addDomainToLimitList')}
+                </Button>
+              )
+            ) : (
+              <Button disabled={!urlInfo.concernedProtocol}>
+                {browser.i18n.getMessage('addDomainToTrackList')}
+              </Button>
+            )}
           </div>
+          <Card className={styles['card']}>
+            <Card.Grid className={styles['grid']}>
+              <Statistic
+                title={browser.i18n.getMessage('timeUsed')}
+                value={
+                  domainState.tracked
+                    ? domainState.currentlyUsedTime > 59
+                      ? (domainState.currentlyUsedTime / 60).toFixed(1)
+                      : domainState.currentlyUsedTime
+                    : '--'
+                }
+                suffix={
+                  domainState.tracked
+                    ? domainState.currentlyUsedTime > 59
+                      ? browser.i18n.getMessage('hour')
+                      : browser.i18n.getMessage('minute')
+                    : undefined
+                }
+              />
+            </Card.Grid>
+            <Card.Grid className={styles['grid']}>
+              <Statistic
+                title={browser.i18n.getMessage('tabOpenedTimes')}
+                value={domainState.tracked ? domainState.openedTimes : '--'}
+                suffix={
+                  domainState.tracked
+                    ? browser.i18n.getMessage('times')
+                    : undefined
+                }
+              />
+            </Card.Grid>
+            <Card.Grid className={styles['grid']} style={{ padding: 0 }}>
+              <Button type="link" icon={<PieChartOutlined />} block>
+                {browser.i18n.getMessage('overview')}
+              </Button>
+            </Card.Grid>
+            <Card.Grid className={styles['grid']} style={{ padding: 0 }}>
+              <Button type="link" icon={<SettingOutlined />} block>
+                {browser.i18n.getMessage('settings')}
+              </Button>
+            </Card.Grid>
+          </Card>
         </>
       ) : (
         <Typography.Text>Loading...</Typography.Text>
---
+++ src/utils/common.ts
@@ -0,0 +1,83 @@
+import { browser } from 'webextension-polyfill-ts';
+
+const host2domain: (host: string) => string = host => {
+  const parts = host.split('.');
+  return parts.length > 1
+    ? `${parts[parts.length - 2]}.${parts[parts.length - 1]}`
+    : parts[0];
+};
+
+export interface urlInfo {
+  concernedProtocol: boolean;
+  protocol: string;
+  host?: string;
+  domain?: string;
+  favIconUrl?: string;
+  displayName: string;
+}
+
+const vendors: Array<string> = ['chrome', 'firefox', 'edge'];
+
+export const getUrlInfoInActiveTab: () => Promise<urlInfo> = async () => {
+  let tabs = await browser.tabs.query({ active: true, currentWindow: true });
+  const url: string = tabs[0].url as string;
+  const protocolReg = /^([A-Za-z]+)(?=:\/{2})/.exec(url);
+  const protocol = protocolReg ? protocolReg[0] : '';
+
+  switch (protocol) {
+    case 'http':
+    case 'https':
+    case 'chrome':
+    // firefox & edge are not tested
+    case 'firefox':
+    case 'edge':
+      const hostReg = /(?<=:\/{2})[^\r\n\t\f\v\/]+(?=\/?)/.exec(url);
+      const host = hostReg ? hostReg[0] : '';
+      const isIP = /^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})(:\d{1,5})?$/.test(
+        host,
+      );
+      const domain = isIP ? host : host2domain(host);
+      const favIconUrl = tabs[0].favIconUrl;
+
+      return {
+        concernedProtocol: true,
+        protocol: protocol,
+        host: host,
+        domain: domain,
+        favIconUrl: favIconUrl && favIconUrl !== '' ? favIconUrl : undefined,
+        displayName: vendors.includes(protocol)
+          ? `${protocol}://${domain}`
+          : domain,
+      };
+    case 'file':
+      return {
+        concernedProtocol: false,
+        protocol: protocol,
+        displayName: protocol.charAt(0).toUpperCase() + protocol.slice(1),
+      };
+    default:
+      return {
+        concernedProtocol: false,
+        protocol: 'other',
+        displayName: 'Other',
+      };
+  }
+};
+
+export interface domainState {
+  tracked: boolean;
+  limited: boolean;
+  maxLimitTime?: number;
+  currentlyUsedTime?: number;
+  openedTimes?: number;
+}
+
+export const getDomainState: (urlInfo: urlInfo) => domainState = urlInfo => {
+  return {
+    tracked: true,
+    limited: true,
+    currentlyUsedTime: 40,
+    maxLimitTime: 70,
+    openedTimes: 999,
+  };
+};
